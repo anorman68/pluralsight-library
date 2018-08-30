15a6cd52decf17116f05f97260aaa3992375122a
anorman68 <anorman68@gmail.com>
2018-08-30 14:23:55 -0400

    after module 6 routing

--- app.js
+++ app.js
@@ -12,9 +12,35 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+// app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
+const nav = [{
+  link: '/books',
+  title: 'Book'
+}, {
+  link: '/authors',
+  title: 'Author'
+}];
+
+
+const bookRouter = require('./src/routes/bookRoutes')(nav);
+
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  // res.sendFile(path.join(__dirname, 'views/index.html'));
+  // res.render('index', { title: 'MyLibrary' });
+  res.render('index', {
+    nav: [{
+      link: '/books',
+      title: 'Books'
+    }, {
+      link: '/authors',
+      title: 'Authors'
+    }],
+    title: 'Library'
+  });
 });
 
 app.listen(port, () => {
