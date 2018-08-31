cf696d2b7057a7f3a9a44abc2a3f6810e4eb08e8
anorman68 <anorman68@gmail.com>
2018-08-31 09:35:26 -0400

    end of module 7 -- added mongodb

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {
-  MongoClient
+  MongoClient, ObjectID
 } = require('mongodb');
 const debug = require('debug')('app:bookRoutes');
 
@@ -40,11 +40,31 @@ function router(nav) {
       const {
         id
       } = req.params;
-      res.render('bookView', {
-        nav,
-        title: 'Library',
-        book: books[id]
-      });
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+
+      (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('Connected correctly to server');
+
+          const db = client.db(dbName);
+
+          const col = await db.collection('books');
+
+          const book = await col.findOne({ _id: new ObjectID(id) });
+          debug(book);
+
+          res.render('bookView', {
+            nav,
+            title: 'Library',
+            book
+          });
+        } catch (err) {
+          debug(err.stack);
+        }
+      }());
     });
   return bookRouter;
 }
