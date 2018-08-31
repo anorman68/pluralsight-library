7933767f45a58badb9d9e34f3b5660af151545f0
anorman68 <anorman68@gmail.com>
2018-08-31 13:37:21 -0400

    end of module 8 -- added passport (not quite working)

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const {
-  MongoClient, ObjectID
+  MongoClient,
+  ObjectID
 } = require('mongodb');
 const debug = require('debug')('app:bookRoutes');
 
 const bookRouter = express.Router();
 
 function router(nav) {
+  bookRouter.use((req, res, next) => {
+    if (req.user) {
+      next();
+    } else {
+      res.redirect('/');
+    }
+  });
   bookRouter.route('/')
     .get((req, res) => {
       const url = 'mongodb://localhost:27017';
@@ -53,7 +61,9 @@ function router(nav) {
 
           const col = await db.collection('books');
 
-          const book = await col.findOne({ _id: new ObjectID(id) });
+          const book = await col.findOne({
+            _id: new ObjectID(id)
+          });
           debug(book);
 
           res.render('bookView', {
